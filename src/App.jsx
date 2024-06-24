5125473f71112115a443b058b84cafe92708c360
John Lai <lai.john.py@gmail.com>
2024-06-24 14:33:56 -0400

    Add Sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
+import SignIn from './components/SignIn';
 
 export default function App() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-semibold mb-4">
-        Base UI + Vite.js + Tailwind CSS deploy test
-      </h1>
-      <span>
-        Created with 💙 by{' '}
-        <a href="https://mui.com" className="link">
-          MUI
-        </a>
-        .
-      </span>
-    </div>
+    <>
+      <h1>TESt</h1>
+      <SignIn />
+    </>
   );
 }
